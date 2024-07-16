a35c4778a96e7038610b87057276b8cb803dc4d7
cristian.sanchez.casd <cristian.sanchez.casd@gmail.com>
2024-07-16 21:18:17 +0000

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    const [selected, setSelected] = useState<string | null>('home')
+    return (
+        <div style={{ bottom: 0 }} className='fixed flex justify-center w-screen gap-5'>
+            {selected === 'home' ? (
+                <svg xmlns="http://www.w3.org/2000/svg" width="3em" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z" />
+                </svg>
+            ) : (
+            <Link to={'/'}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="3em" onClick={() => setSelected('home')} viewBox="0 0 32 32">
+                <path fill="currentColor" d="M16.612 2.214a1.01 1.01 0 0 0-1.242 0L1 13.419l1.243 1.572L4 13.621V26a2.004 2.004 0 0 0 2 2h20a2.004 2.004 0 0 0 2-2V13.63L29.757 15L31 13.428ZM18 26h-4v-8h4Zm2 0v-8a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v8H6V12.062l10-7.79l10 7.8V26Z" />
+            </svg>
+            </Link>
+        )}
+            {
+                selected === 'profile' ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="3em" viewBox="0 0 24 24">
+                        <path fill="currentColor" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076A9.959 9.959 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.958 9.958 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22a9.947 9.947 0 0 0 5.675-1.765a10.055 10.055 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd" />
+                    </svg>
+                ) : (
+
+                    <svg onClick={() => setSelected('profile')} xmlns="http://www.w3.org/2000/svg" width="3em" viewBox="0 0 24 24">
+                        <g fill="none">
+                            <path stroke="currentColor" strokeWidth="1.5" d="M21 12a8.958 8.958 0 0 1-1.526 5.016A8.991 8.991 0 0 1 12 21a8.991 8.991 0 0 1-7.474-3.984A9 9 0 1 1 21 12Z" /><path fill="currentColor" d="M13.25 9c0 .69-.56 1.25-1.25 1.25v1.5A2.75 2.75 0 0 0 14.75 9zM12 10.25c-.69 0-1.25-.56-1.25-1.25h-1.5A2.75 2.75 0 0 0 12 11.75zM10.75 9c0-.69.56-1.25 1.25-1.25v-1.5A2.75 2.75 0 0 0 9.25 9zM12 7.75c.69 0 1.25.56 1.25 1.25h1.5A2.75 2.75 0 0 0 12 6.25zM5.166 17.856l-.719-.214l-.117.392l.267.31zm13.668 0l.57.489l.266-.31l-.117-.393zM9 15.75h6v-1.5H9zm0-1.5a4.752 4.752 0 0 0-4.553 3.392l1.438.428A3.252 3.252 0 0 1 9 15.75zm3 6a8.23 8.23 0 0 1-6.265-2.882l-1.138.977A9.73 9.73 0 0 0 12 21.75zm3-4.5c1.47 0 2.715.978 3.115 2.32l1.438-.428A4.752 4.752 0 0 0 15 14.25zm3.265 1.618A8.23 8.23 0 0 1 12 20.25v1.5a9.73 9.73 0 0 0 7.403-3.405z" /></g>
+                    </svg>
+                )
+            }
+        </div>
+    )
+}
+
+export default Navbar
